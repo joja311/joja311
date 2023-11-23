577f7bf20285d4ded007acbc568167c0ba9684f0
joja311 <151573326+joja311@users.noreply.github.com>
2023-11-23 07:08:52 +0000

    Update my first project.py
    -Documentation and error handling

--- my first project.py
+++ my first project.py
@@ -3,79 +3,104 @@ import datetime
 task_list = []
 
 class Task:
+    """
+    Represents a general task.
+    """
     def __init__(self, name, type, time):
+        """
+        Initializes a Task instance.
+
+        Parameters:
+        - name (str): The name of the task.
+        - type (str): The type of the task.
+        - time (str): The start time of the task.
+        """
         self.name = name
         self.type = type
         self.time = time
         self.completed = False
 
     def __str__(self):
+        """
+        Returns a string representation of the task.
+        """
         status = "Completed" if self.completed else "Pending"
         return f"{self.name}, starts at {self.time}, type: {self.type} task ({status})"
 
 class PeriodicTask(Task):
+    """
+    Represents a periodic task.
+    """
     def __init__(self, name, type, time, period):
+        """
+        Initializes a PeriodicTask instance.
+
+        Parameters:
+        - name (str): The name of the task.
+        - type (str): The type of the task.
+        - time (str): The start time of the task.
+        - period (int): The period of the task in minutes.
+        """
         super().__init__(name, type, time)
         self.period = period
 
     def __str__(self):
+        """
+        Returns a string representation of the periodic task.
+        """
         status = "Completed" if self.completed else "Pending"
         return f"{self.period} minutes of {self.name}, starts at {self.time}, type: {self.type} task ({status})"
 
 def to_do():
-    task_num = int(input('How many tasks do you wish to create: '))
+    """
+    Takes user input to create tasks and adds them to the task list.
+    """
+    try:
+        task_num = int(input('How many tasks do you wish to create: '))
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        return
+
     for i in range(1, task_num + 1):
         task_name = input(f'Enter task {i} name: ')
         task_type = input(f'Enter task {i} type: ')
         task_time = input(f'Enter task {i} start time: ')
-        task_period_ask = input(f'Do you want to add a period for task {i}? (1 for yes and 0 for no) ')
-        if task_period_ask.strip() == '1':
-            task_period = input('For how many minutes? ')
-            task_add = PeriodicTask(task_name, task_type, task_time, task_period)
-        else:
-            task_add = Task(task_name, task_type, task_time)
-        task_list.append(task_add)
+
+        try:
+            task_period_ask = input(f'Do you want to add a period for task {i}? (1 for yes and 0 for no) ')
+            if task_period_ask.strip() == '1':
+                task_period = int(input('For how many minutes? '))
+                task_add = PeriodicTask(task_name, task_type, task_time, task_period)
+            else:
+                task_add = Task(task_name, task_type, task_time)
+            task_list.append(task_add)
+        except ValueError:
+            print("Invalid input. Please enter a valid number for the period.")
+            return
 
 def complete_task():
+    """
+    Marks a task as completed based on user input.
+    """
     print("Incomplete Tasks:")
     for i, task in enumerate(task_list, start=1):
         if not task.completed:
             print(f"{i}. {task}")
 
-    task_n = int(input("Enter the task number to mark as completed: "))
     try:
-        task_list[task_n - 1].completed = True
-        print(f"Task {task_n} marked as completed.")
-    except IndexError:
-        print("Invalid task number.")
-
-def menu():
-    print('\n\nMENU:')
-    for i, tsk in enumerate(task_list, start=1):
-        print(f'{i}. {tsk}')
-
-    print('OPTIONS:')
-    print('1. Add tasks')
-    print('2. Complete task')
-    print('3. View completed tasks')
-    print('4. Exit')
-
-    choice = int(input('Enter choice number (1, 2, 3, or 4): '))
-
-    if choice == 1:
-        to_do()
-    elif choice == 2:
-        complete_task()
-    elif choice == 3:
-        view_completed_tasks()
-    elif choice == 4:
-        exit()
-    else:
-        print("Invalid choice. Please select a valid option.")
-
-    menu()
+        task_n = int(input("Enter the task number to mark as completed: "))
+        if 1 <= task_n <= len(task_list):
+            task_list[task_n - 1].completed = True
+            print(f"Task {task_n} marked as completed.")
+        else:
+            print("Invalid task number.")
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
 
 def view_completed_tasks():
+    """
+    Displays a list of completed tasks.
+    """
     completed_tasks = [task for task in task_list if task.completed]
     if not completed_tasks:
         print('No completed tasks.')
@@ -84,5 +109,38 @@ def view_completed_tasks():
         for i, task in enumerate(completed_tasks, start=1):
             print(f'{i}. {task}')
 
+def menu():
+    """
+    Displays the main menu and handles user choices.
+    """
+    while True:
+        print('\n\nMENU:')
+        for i, tsk in enumerate(task_list, start=1):
+            print(f'{i}. {tsk}')
+
+        print('OPTIONS:')
+        print('1. Add tasks')
+        print('2. Complete task')
+        print('3. View completed tasks')
+        print('4. Exit')
+
+        try:
+            choice = int(input('Enter choice number (1, 2, 3, or 4): '))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            continue
+
+        if choice == 1:
+            to_do()
+        elif choice == 2:
+            complete_task()
+        elif choice == 3:
+            view_completed_tasks()
+        elif choice == 4:
+            exit()
+        else:
+            print("Invalid choice. Please select a valid option.")
+
 if __name__ == "__main__":
     menu()
+
